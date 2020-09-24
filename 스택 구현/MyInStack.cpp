008995ee1b7a83fa54fdecf4d60c66aec90008ef
seoyeon <67668501+SEOYEONGO@users.noreply.github.com>
2020-09-24 18:59:03 +0900

    Add files via upload

---
+++ 스택 구현/MyInStack.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class MyInStack{
+	int p[10];
+	int tos;
+public:
+	MyInStack(){
+		tos=0;
+	}
+	bool push (int n){
+		if (tos==10) return false;
+		else {
+			p[tos++]=n;
+			return true;
+		}
+	}
+	bool pop (int &n){
+		if (tos==0) return false;
+		else {
+			n=p[--tos];
+			return true;
+		}
+	}
+}; 
+
+int main (){
+	MyInStack a;
+	for (int i=0;i<11;i++){
+		if (a.push(i)) cout<<i<<' ';
+		else cout<<endl<<i+1<<" 번째 stack full"<<endl;
+	}
+	int n;
+	for (int i=0;i<11;i++){
+		if (a.pop(n)) cout<<n<<' ';
+		else cout<<endl<<i+1<<" 번째 stack empty"; 
+	}
+	cout<<endl;
+}
