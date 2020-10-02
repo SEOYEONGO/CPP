64576a5bf5ab72d6a220450d0f21a224664c52cb
seoyeon <67668501+SEOYEONGO@users.noreply.github.com>
2020-10-02 12:41:31 +0900

    Add files via upload

---
+++ NamedCircle/NamedCircle.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Circle{
+	int radius;
+public:
+	Circle (int radius=0) {this->radius=radius;}
+	int getRadius(){return radius;}
+	void setRadius(int radius) { this->radius=radius;}
+	double getArea(){return 3.14*radius*radius;}
+};
+
+class NamedCircle : public Circle {
+	string name;
+public:
+	NamedCircle(int radius, string name){
+		setRadius(radius);
+		this->name=name;
+	}
+	void show(){
+		cout<<"반지름이 "<<getRadius()<<"인 "<<name;
+	}
+};
+
+int main (){
+	NamedCircle waffle(3, "waffle");
+	waffle.show();
+}
